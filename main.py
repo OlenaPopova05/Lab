bd738d030c7a1bd07cd375011a956f7f0399b1f8
Olena Popova <opopova@kse.org.ua>
2024-06-02 22:55:21 +0300

    open cv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 
 def plot_objects(points1, points2, title1, title2):
@@ -34,9 +35,19 @@ def object_rotation(points, angle):
     return np.dot(rotation_matrix, points.T).T
 
 
+def object_rotation_cv(points, angle):
+    rotation_matrix = cv2.getRotationMatrix2D((0, 0), angle, 1)
+    return cv2.transform(points.reshape(-1, 1, 2), rotation_matrix).reshape(-1, 2)
+
+
 def object_scaling(points, scale):
     scaling_matrix = np.array([[scale, 0], [0, scale]])
     return np.dot(points, scaling_matrix)
+
+
+def object_scaling_cv(points, scale):
+    scaling_matrix = np.array([[scale, 0], [0, scale]])
+    return cv2.transform(points.reshape(-1, 1, 2), scaling_matrix).reshape(-1, 2)
 
 
 def object_axis_reflection(points, axis):
@@ -47,6 +58,14 @@ def object_axis_reflection(points, axis):
     return np.dot(points, reflection_matrix)
 
 
+def object_axis_reflection_cv(points, axis):
+    if axis == 'x':
+        reflection_matrix = np.array([[1, 0], [0, -1]])
+    else:
+        reflection_matrix = np.array([[-1, 0], [0, 1]])
+    return cv2.transform(points.reshape(-1, 1, 2), reflection_matrix).reshape(-1, 2)
+
+
 def object_shear(points, shear_factor, axis):
     if axis == 'x':
         shear_matrix = np.array([[1, shear_factor], [0, 1]])
@@ -55,8 +74,20 @@ def object_shear(points, shear_factor, axis):
     return np.dot(shear_matrix, points.T).T
 
 
+def object_shear_cv(points, shear_factor, axis):
+    if axis == 'x':
+        shear_matrix = np.array([[1, shear_factor], [0, 1]])
+    else:
+        shear_matrix = np.array([[1, 0], [shear_factor, 1]])
+    return cv2.transform(points.reshape(-1, 1, 2), shear_matrix).reshape(-1, 2)
+
+
 def custom_transformation(points, transformation_matrix):
     return np.dot(transformation_matrix, points.T).T
+
+
+def custom_transformation_cv(points, transformation_matrix):
+    return cv2.transform(points.reshape(-1, 1, 2), transformation_matrix).reshape(-1, 2)
 
 
 pyramid = np.array([
@@ -91,7 +122,7 @@ trapezoid_rotated = object_rotation(trapezoid, 90)
 plot_objects(trapezoid, trapezoid_rotated, 'Trapezoid', 'Rotated Trapezoid')
 
 triangle_rotated = object_rotation(triangle, 90)
-plot_objects(triangle, triangle_rotated, 'Triangle','Rotated Triangle')
+plot_objects(triangle, triangle_rotated, 'Triangle', 'Rotated Triangle')
 
 scale_factor = 2
 
@@ -135,3 +166,26 @@ plot_objects(trapezoid, trapezoid_custom_transformed, 'Trapezoid', 'Custom Trans
 
 triangle_custom_transformed = custom_transformation(triangle, custom_transformation_matrix)
 plot_objects(triangle, triangle_custom_transformed, 'Triangle', 'Custom Transformed Triangle')
+
+
+
+
+triangle_rotated_cv = object_rotation_cv(triangle, 90)
+plot_objects(triangle, triangle_rotated_cv, 'Triangle', 'Rotated Triangle (OpenCV)')
+
+triangle_scaled_cv = object_scaling_cv(triangle, scale_factor)
+plot_objects(triangle, triangle_scaled_cv, 'Triangle', f'Scaled Triangle (scale factor={scale_factor}) (OpenCV)')
+
+triangle_reflected_x_cv = object_axis_reflection_cv(triangle, 'x')
+plot_objects(triangle, triangle_reflected_x_cv, 'Triangle', 'Reflected Triangle (X-axis) (OpenCV)')
+triangle_reflected_y_cv = object_axis_reflection_cv(triangle, 'y')
+plot_objects(triangle, triangle_reflected_y_cv, 'Triangle', 'Reflected Triangle (Y-axis) (OpenCV)')
+
+triangle_sheared_x_cv = object_shear_cv(triangle, shear_factor, 'x')
+plot_objects(triangle, triangle_sheared_x_cv, 'Triangle', f'Sheared Triangle (X-axis, shear factor={shear_factor}) (OpenCV)')
+triangle_sheared_y_cv = object_shear_cv(triangle, shear_factor, 'y')
+plot_objects(triangle, triangle_sheared_y_cv, 'Triangle', f'Sheared Triangle (Y-axis, shear factor={shear_factor}) (OpenCV)')
+
+
+triangle_custom_transform_cv = custom_transformation_cv(triangle, custom_transformation_matrix)
+plot_objects(triangle, triangle_custom_transform_cv, 'Triangle', 'Custom Transformed Triangle (OpenCV)')
